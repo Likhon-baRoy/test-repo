b89f63d9a40195330d9df1f8a6aed604e8418f4a
Likhon Barai <likhonhere007@gmail.com>
2024-04-06 23:37:44 +0600

    emacs added

---
+++ etc/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company-wordfreq company-shell toc-org org-bullets eglot yasnippet-snippets yasnippet company-posframe company-statistics company-quickhelp company-box emmet-mode modern-cpp-font-lock flycheck-popup-tip flycheck ligature dashboard minions doom-modeline doom-themes solaire-mode rainbow-delimiters rainbow-mode comment-dwim-2 try alert emojify beacon highlight-numbers volatile-highlights highlight-indent-guides olivetti fancy-battery ivy-rich ivy-avy ivy-prescient prescient ivy-posframe wgrep flx counsel ivy auto-package-update aggressive-indent drag-stuff expand-region goto-chg undo-tree ace-window eyebrowse helpful which-key git-timemachine git-gutter-fringe git-gutter magit uniquify-files avy nerd-icons benchmark-init gcmh delight no-littering use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(show-paren-match ((t (:background "#3c3836" :foreground "white" :weight extra-bold))))
+ '(show-paren-mismatch ((((class color)) (:background "red" :foreground "white")))))
